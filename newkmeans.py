657ed1ec487d1bf413f0b98cbc76d6f9f3274f9f
ai016 <ai016@cip52.pool>
2018-01-19 02:18:26 +0100

    q1 done

--- newkmeans.py
+++ newkmeans.py
@@ -1,19 +1,15 @@
 import numpy as np
 import cv2
 
-def kmeans_pic(inpicfile, outpicfile, k):
-	img = cv2.imread(inpicfile)
-	vect = img.reshape((-1,3))
-	vect = np.float32(vect) # convert to float for k-means calculation
+img = cv2.imread('grass2.jpg')
+vect = img.reshape((-1,3))
+vect = np.float32(vect) # convert to float for k-means calculation
+k=8
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+ret, label, centroids = cv2.kmeans(vect, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
-	criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-	_, label, centroids = cv2.kmeans(vect, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+centroids = np.uint8(centroids) # convert back to image values
+outpic = centroids[label.flatten()]
+outpic2 = outpic.reshape((img.shape))
+cv2.imwrite('newgrass3.jpg', outpic2)
 
-	centroids = np.uint8(centroids) # convert back to image values
-	outpic = centroids[label.flatten()]
-	outpic2 = outpic.reshape((img.shape))
-	cv2.imwrite(outpicfile, outpic2)
-
-kmeans_pic('grass2.jpg', 'newgrass3.jpg', 3)
-#kmeans_pic('grass2.jpg', 'outgrass5.jpg', 5)
-#kmeans_pic('grass2.jpg', 'outgrass8.jpg', 8)
