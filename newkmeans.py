ebb16abb9faf14997d94cb794c2ab3e545c947ed
ai016 <ai016@cip52.pool>
2018-01-19 02:22:02 +0100

    q1 done

--- newkmeans.py
+++ newkmeans.py
@@ -16,4 +16,7 @@ ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
 center = np.uint8(center)
 res = center[label.flatten()]
 res2 = res.reshape((img.shape))
+
+#cv2.imshow('res2',res2)
+cv2.waitKey(0)
 cv2.imwrite('oldgrass8.jpg',res2)
